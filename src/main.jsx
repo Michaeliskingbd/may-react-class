81f898ee712f1bdf08f0c68a5a356ea41d69e396
Your Name <michaeliskingbd@gmail.com>
2025-09-24 10:27:41 +0100

    hosting

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { UserProvider } from "./context/Usercontext.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
