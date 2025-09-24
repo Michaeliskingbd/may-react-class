b32efdc403b6e6595288b778ab386d54934ed6a6
Your Name <michaeliskingbd@gmail.com>
2025-09-24 10:10:50 +0100

    hosting2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { UserProvider } from "./context/Usercontext.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
@@ -10,11 +10,11 @@ import store from "./redux/store.js";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <UserProvider>
           <App />
         </UserProvider>
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>
   </Provider>
 );
